5508e91250fd683c45156d4bcf1d89a6f79f0cdb
Kamil Dybicz <kamil@secretescapes.com>
2023-08-17 20:01:32 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -4,12 +4,6 @@ red=$'\e[31m'
 green=$'\e[32m'
 reset=$'\e[0m'
 
-echo "${GIT_CLONE_COMMIT_HASH}"
-echo "${client_key}"
-echo "${pipeline}"
-echo "${build}"
-echo "${commits}"
-
 body='{
   "client_key": "'${client_key}'",
   "pipeline": "'${pipeline}'",
@@ -47,35 +41,3 @@ else
   status="$(echo $res | jq '.status' | tr -d '"')"
   echo "Status = " $status
 fi
-
-# {
-#     "detail": "Unknown build 3435 for client secret-escapes and pipeline app-android",
-#     "status": 400,
-#     "title": "Bad Request",
-#     "type": "about:blank"
-# }
-
-# {
-#     "detail": "Invalid credentials",
-#     "status": 401,
-#     "title": "Unauthorized",
-#     "type": "about:blank"
-# }
-
-# {
-#     "deployment": {
-#         "branch_name": "release/5.4.5",
-#         "build": "3435",
-#         "calculate_commits_in_build": false,
-#         "client_key": "secret-escapes",
-#         "commenced_at": null,
-#         "commits": [
-#             "asdf"
-#         ],
-#         "context": null,
-#         "deployed_at": "2023-08-07T10:13:12+00:00",
-#         "pipeline": "app-android",
-#         "status": "success"
-#     },
-#     "status": "OK"
-# }
